1b52e751e4443b711b25a1f5ea89d72ab91fd531
Will Locklier <will.locklier@gmail.com>
2020-12-18 12:07:56 -0500

    [Welcome] Add Welcome component and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Welcome from "./components/Welcome";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("render the Welcome header", () => {
+  render(<Welcome />);
+  const h1Element = screen.getByText(/i/);
 });
